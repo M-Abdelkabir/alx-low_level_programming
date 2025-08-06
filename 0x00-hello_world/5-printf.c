54c0f231c935a418347823cf3b24687d8d301396
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-08-06 21:06:13 +0100

    why we use printf in a simple text!!!

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - The main function
+ * Return: Return of main with the value 0
+ */
+int main(void)
+{
+	char text[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s",text);
+
+	return (0);
+}
