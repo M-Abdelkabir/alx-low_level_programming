1493af427cd5f45abd3575162c7aa184bc99b349
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-08-06 20:51:26 +0100

    why we use printf for just print a simple text. Use puts is more fast

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * return - return an integer with value of 0
+ */
+int main(){
+	char text[] = "Programming is like building a multilingual puzzle\0";
+
+	puts(text);
+	return 0;
+}
