278081b490c289b8c3335ccc34448a2d62c58516
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-08-06 22:12:36 +0100

    assembly code (Intel syntax) of a C code

--- 0x00-hello_world/0-main.s
+++ 0x00-hello_world/0-main.s
@@ -8,14 +8,23 @@
 	.globl	main
 	.type	main, @function
 main:
+.LFB0:
+	.cfi_startproc
 	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
 	mov	rbp, rsp
-	mov	edi, OFFSET FLAT:.LC0
+	.cfi_def_cfa_register 6
+	lea	rax, .LC0[rip]
+	mov	rdi, rax
 	mov	eax, 0
-	call	printf
+	call	printf@PLT
 	mov	eax, 0
 	pop	rbp
+	.cfi_def_cfa 7, 8
 	ret
+	.cfi_endproc
+.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (Debian 14.2.0-19) 14.2.0"
 	.section	.note.GNU-stack,"",@progbits
