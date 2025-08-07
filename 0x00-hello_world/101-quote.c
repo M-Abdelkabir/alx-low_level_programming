9e7be2263bb432de43c2e6e88c94b36e0a9811d4
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-08-07 22:13:12 +0100

    I love know how it went in begning

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,10 @@
  */
 int main(void)
 {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char msg1[] = "and that piece of art is useful\"";
+	const char msg2[] = " - Dora Korpar, 2015-10-19\n";
 
-    write(2, msg, sizeof(msg) - 1);
-    return 1;
+	write(2, msg1, sizeof(msg1) - 1);
+	write(2, msg2, sizeof(msg2) - 1);
+	return (1);
 }
