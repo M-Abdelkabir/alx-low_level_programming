5fff122074e9e22905f9744f32bbffec2d4147ea
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-08-30 19:53:33 +0100

    let's do it like we want, we don't care about the advanced task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Alphabet with just two putchar
+ * Return: Always 0 hhhh
+ */
+
+int main(void)
+{
+	register int i = 0;
+	register int j = 0;
+
+	for (; i < 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar('0' + i % 10);
+			putchar('0' + i / 10);
+			putchar(' ');
+			putchar('0' + j % 10);
+			putchar('0' + j / 10);
+			if (i == 98)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
