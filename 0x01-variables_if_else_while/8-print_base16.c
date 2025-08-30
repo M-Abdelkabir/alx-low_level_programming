31bf6ee1ca9510b127da3beb1a1303a5bbef70f7
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-08-30 18:59:50 +0100

    let's do it like we want

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Alphabet with just two putchar
+ * Return: Always 0 hhhh
+ */
+
+int main(void)
+{
+	char text[] = "0123456789abcdef\n";
+	int n = sizeof(text) - 1;
+
+	register int i = 0;
+
+	for (; i < n; i++)
+		putchar(text[i]);
+	return (0);
+}
+
