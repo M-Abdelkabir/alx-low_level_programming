b8f154211af7203195ed4c421eb60d344b687273
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-09-14 17:57:39 +0100

    let's do it like we want, it's so easy for a software engineer from alx and university cady ayad

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,16 +6,16 @@
  */
 void print_chessboard(char (*a)[8])
 {
-    char (*ligne)[8] = a;
-    char *colonne;
+	char (*ligne)[8] = a;
+	char *colonne;
 
-    while (ligne <= a + 8)
-    {
-        for (colonne = *ligne; colonne < *ligne + 8; colonne++)
-        {
-            putchar(*colonne);
-        }
-        putchar('\n');
-        ligne++;
-    }
+	while (ligne < a + 8)
+	{
+		for (colonne = *ligne; colonne < *ligne + 8; colonne++)
+		{
+			putchar(*colonne);
+		}
+		putchar('\n');
+		ligne++;
+	}
 }
