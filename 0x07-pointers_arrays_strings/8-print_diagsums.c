2bbf2af48b34f2fc86f32fdaf2bf30dcd00dd1b9
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-09-14 18:37:57 +0100

    let's do it like we want, it's so easy for a software engineer from alx and university cady ayad

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@ void print_diagsums(int *a, int size)
 {
 	int left = 0, right = 0, i;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		left += *(a + i * (size + 1));
 		right += *(a + (i + 1) * (size - 1));
