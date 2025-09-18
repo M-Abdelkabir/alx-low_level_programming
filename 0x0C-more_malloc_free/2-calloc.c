7896ac392ec468d0576efb90ee9e4ae034e7ba60
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-09-18 14:14:05 +0100

    let's finish what we start

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,12 +11,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-        s[i] = b;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 
-    return (s);
+	return (s);
 }
 /**
  * _calloc - Allocates memory for an array and initializes it to zero
@@ -27,18 +27,18 @@ char *_memset(char *s, char b, unsigned int n)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
-    unsigned int total_size;
+	void *ptr;
+	unsigned int total_size;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    total_size = nmemb * size;
-    ptr = malloc(total_size);
-    if (ptr == NULL)
-        return (NULL);
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+	if (ptr == NULL)
+		return (NULL);
 
-    _memset(ptr, 0, total_size);
+	_memset(ptr, 0, total_size);
 
-    return (ptr);
+	return (ptr);
 }
