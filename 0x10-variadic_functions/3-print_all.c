d2aa1ad0d4b12bb64deae4279c6fbda64c0c3ade
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-09-30 18:39:59 +0100

    let's do it by our hand

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,48 +8,43 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0;
-    char *str;
-    char current_char;
+	va_list args;
+	int i = 0;
+	char *str;
+	char current_char;
 
-    va_start(args, format);
-
-    while (format && format[i])
-    {
-        current_char = format[i];
-        
-        switch (current_char)
-        {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (str == NULL)
+	va_start(args, format);
+	while (format && format[i])
+	{
+		current_char = format[i];
+		switch (current_char)
 		{
-                    printf("(nil)");
-		    break;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+			default:
+				i++;
+				continue;
 		}
-		printf("%s", str);
-                break;
-            default:
-                i++;
-                continue;
-        }
-        
-        if (format[i + 1] != '\0')
-            printf(", ");
-        
-        i++;
-    }
-
-    va_end(args);
-    printf("\n");
+		if (format[i + 1] != '\0')
+			printf(", ");
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
