d30e48140a45fa5b64cb18b730b069348e10de7f
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-10-12 18:47:37 +0100

    That's my mom tell me every time .You must clean your place before going

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - jfhghiugh
+ * @head: jiuhgiuherguh
+ *
+ * Return: jnvihgiuh
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp1 = *head, *temp;
+
+	while (temp1 != NULL)
+	{
+		temp = temp1;
+		temp1 = temp1->next;
+		free(temp);
+	}
+	*head = NULL;
+}
