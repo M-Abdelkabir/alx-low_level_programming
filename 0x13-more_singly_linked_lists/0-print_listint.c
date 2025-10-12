0323e62e9b2415353db678142985935be147f4c6
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-10-12 16:53:20 +0100

    we are know professionelle in using linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,12 +12,13 @@ size_t print_listint(const listint_t *h)
 	int count = 0;
 
 	if (h == NULL)
-		return 0;
+		return (0);
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
+
 	return (count);
 }
