81efcad481fc5898991293cd65a705cc1f44ac45
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-10-14 14:30:59 +0100

    The head will be the tail. This is the effect of the kings

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - kjbnih
+ * @head: kjbngijgn
+ *
+ * Return: iuthgijtrjgio
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *next;
+
+	if (head == NULL || *head == NULL)
+		return(NULL);
+	if ((*head)->next == NULL)
+		return(*head);
+	next = (*head)->next;
+	(*head)->next = NULL;
+	while (next != NULL)
+	{
+		prev = *head;
+		*head = next;
+		next = next->next;
+		(*head)->next = prev;
+	}
+
+	return(*head);
+}
