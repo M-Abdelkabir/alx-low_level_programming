edc14b207d366a9f2107c4726375be3de4a716e3
M-Abdelkabir <m.abdelkabir.engineer@gmail.com>
2025-10-15 19:26:00 +0100

    Know the loop can't play with my print function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,16 +9,49 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	int count = 0;
+	size_t count = 0;
+	int found = 0;
+	const listint_t *one = head, *tow = head, *loop_start = NULL;
 
 	if (head == NULL)
-		exit(98);
-	while (head != NULL)
+		return (0);
+	while (tow != NULL && tow->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
-		count++;
+		one = one->next;
+		tow = tow->next->next;
+		if (tow == one)
+		{
+			found = 1;
+			break;
+		}
 	}
+	
+	if (found)
+	{
+		one = head;
+		while (tow != one)
+		{
+			one = one->next;
+			tow = tow->next;
+		}
+		loop_start = tow;
+	}
+	found = 0;
+	one = head;
+	while (one)
+	{
+		printf("[%p] %d\n", (void *)one, one->n);
+		count++;
+		if (one == loop_start)
+			found = 1;
+		if (found && one->next == loop_start)
+		{
+			printf("-> [%p] %d\n",(void *)loop_start, loop_start->n);
+			break;
+		}
+		one = one->next;
+	}
+
 
 	return (count);
 }
